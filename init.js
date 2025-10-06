dde57631bb7173830e20987af79c02ab16063ab6
naldo1989 <58909547+naldo1989@users.noreply.github.com>
2025-10-06 12:53:08 -0300

    Update init.js

--- init.js
+++ init.js
@@ -11,16 +11,26 @@ async function init() {
     );
   `);
 
-  await pool.query(`
+    await pool.query(`
     CREATE TABLE IF NOT EXISTS registros (
       id SERIAL PRIMARY KEY,
+      usuario_id INTEGER REFERENCES usuarios(id) ON DELETE CASCADE,
       escuela VARCHAR(100) NOT NULL,
-      mesa VARCHAR(20) NOT NULL,
-      votos INT DEFAULT 0,
-      ultima_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      mesa VARCHAR(50) NOT NULL,
+      fecha TIMESTAMP DEFAULT NOW()
       UNIQUE(escuela, mesa)
     );
   `);
+     await pool.query(`
+ CREATE TABLE IF NOT EXISTS votos (
+    id SERIAL PRIMARY KEY,
+    registro_id INTEGER REFERENCES registros(id) ON DELETE CASCADE,
+    nro_orden VARCHAR(20) NOT NULL,
+    total_votos INTEGER GENERATED ALWAYS AS (votos_validos + votos_nulos + votos_blancos) STORED,
+    fecha_carga TIMESTAMP DEFAULT NOW()
+);
+  `);
+
 
   console.log("✅ Tablas creadas");
   process.exit(0);
