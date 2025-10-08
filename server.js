0342c3b5c2b81dff52e129262576ea96769d7280
naldo1989 <58909547+naldo1989@users.noreply.github.com>
2025-10-08 09:42:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const pool = new Pool({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(
   session({
